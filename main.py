ed87da1108e1ddef0a9f7ec5ac426342c51950b5
apolo74 <apolo74@gmail.com>
2024-07-16 11:13:45 +0200

    Separating the front- and back-ends

--- main.py
+++ main.py
@@ -16,22 +16,9 @@ Created:  2024-07-09
 
 import os
 import requests
-
 import gradio as gr
-from dataclasses import dataclass
 
-from langchain_community.chat_models import ChatOllama
-from langchain_community.embeddings import OllamaEmbeddings
-
-from langchain_community.vectorstores import FAISS
-from langchain_community.document_loaders import PyMuPDFLoader
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.runnables import RunnablePassthrough
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.storage import InMemoryByteStore
-# from langchain.docstore.document import Document
-from faiss import IndexFlatL2
+from assistant import Assistant
 
 css = """
 .container {
@@ -43,122 +30,7 @@ css = """
 }
 """
 #col { height: calc(100vh - 112px - 16px) !important; }
-
-# ======================== Class: LLM Assistant ========================
-@dataclass
-class Assistant:
-    """ Assistant Class"""
-    #docs: list
-    #pdf_path: str
-    #index_path: str
-    #k:int = DEFAULT_K
-    with_context: bool = False
-
-    def __post_init__(self):
-        """
-        Initializes an instance of the class with the given parameters.
-        """
-        self.model_name = 'phi3:latest'
-
-        self.llm = ChatOllama( model = self.model_name )
-        self.emb = OllamaEmbeddings( model = "mxbai-embed-large" )
-        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
-
-        dimensions: int = len(self.emb.embed_query("dummy"))
-        self.index = FAISS(embedding_function=self.emb, 
-                           index=IndexFlatL2(dimensions), 
-                           docstore=InMemoryByteStore(), 
-                           index_to_docstore_id={}
-                        )
-
-    def get_models_list( self, models ):
-        """Shows a list of available LLMs and returns the user's selection .
-        Args:
-            List: A List of available Models from Ollama's server list
-        Returns:
-            List: An alphabetically ordered list with Ollama's available models.
-        """
-        models_list = []
-        for ix, model in enumerate(models):
-            models_list.append(model['name'])
-
-        return sorted( models_list )
-
-    def change_model(self, model):
-        self.model_name = model
-        self.llm = ChatOllama(model=self.model_name)
-
-        print('Working with: ', self.llm)
-
-        return
-
-    def ingest_pdf( self, file ):
-        """Transforms your PDF(s) into vector format and splits it(them) into chunks.
-        Args:
-            String: Path to a file or a directory
-        Returns:
-            List: A list with chunked Documents
-        """
-        print(f'Loading {file} ...', end='')
-        loader = PyMuPDFLoader(file)
-        print(' done!')
-        
-        pages = loader.load_and_split( self.text_splitter )
-
-        self.index = FAISS.from_documents( pages, self.emb )
-
-        self.with_context = True
-
-        return
-
-    def clear_pdf(self):
-        self.with_context = False
-
-    def respond(self, question, history):
-
-        if self.with_context:
-
-            PROMPT_TEMPLATE = """Use the following pieces of context to answer the question at the end. 
-            If you don't know the answer, just say that you don't know, don't try to make up an answer. 
-            Use three sentences maximum. Keep the answer as concise as possible.
-            {context}
-            Question: {question}
-            Helpful Answer:"""
-            prompt = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
-            
-            '''
-            prompt = ChatPromptTemplate.from_messages([
-                ("system", "If you don't know the answer, just say that you don't know. Keep the answer as concise as possible. Answer based only on the following context:\n<Documents>\n{context}\n</Documents>",),
-                ("human", question)
-            ])
-            '''
-            chain = (
-                {
-                    "context": self.index.as_retriever(), 
-                    "question": RunnablePassthrough(),
-                }
-                | prompt
-                | self.llm
-                | StrOutputParser()
-            )
-            output = ""
-            for chunk in chain.stream( question ):
-                output = output + chunk
-                yield output
-        else:
-            #    ("system", "You are a technical assistant. Answer with consice but informative formal tone to the given question! If you don't know the answer, just say that you don't know. "),
-            prompt = ChatPromptTemplate.from_messages([
-                ("system", "You are a friendly assistant called Nobody. Answer with a casual tone to the given question! If you don't know the answer, just say that you don't know. "),
-                ("human", question)
-            ])
-            chain = prompt | self.llm
-            output = ""
-            for chunk in chain.stream( {'text': prompt} ):
-                output = output + chunk.content
-                yield output
-
-
-
+'''
 def get_model( models ):
     """Shows a list of available LLMs and returns the user's selection .
     Args:
@@ -213,7 +85,7 @@ def get_pdf_langchain( path ):
     print('Documents: ', len(documents))
 
     return pages
-
+'''
 def main_loop():
     """Main loop where all magic happens!
     Args:
@@ -228,20 +100,21 @@ def main_loop():
     # Define UI
     PLACE_HOLDER = "Ask me something about your docs!"
     with gr.Blocks(css=css) as all_blocks:
-        with gr.Column(elem_classes=["container"]):
-            gr.Markdown(
-                """
-                # Boris's First Chatbot!
-                Upload a PDF and start asking questions about it, enjoy!
-                """)    
+        with gr.Column(elem_classes=["container"]):   
             with gr.Row():
                 with gr.Column(scale=1, min_width=200):
                     with gr.Row():
-                        dd_model = gr.Dropdown(models, value=assistant.model_name, label="Models", interactive=True)
-                        dd_embedder = gr.Dropdown(["nomic", "phi3"], label="Embedders")
+                        gr.Markdown("""
+                                        # <div style="text-align: right"> YARS </div>
+                                        <div style="text-align: right"> Yet Another RAG Script</div>
+                                    """)
                     with gr.Row():
                         tb_file = gr.File(label="File", file_count='single', file_types=['.pdf'])
-                        tb_url = gr.Textbox(label="URL")
+                        # tb_url = gr.Textbox(label="URL")
+                    with gr.Row():
+                        dd_model = gr.Dropdown(models, value=assistant.model_name, label="Models", interactive=True)
+                        dd_embedder = gr.Dropdown(["nomic", "phi3"], label="Embedders")
+                        sl_temperature = gr.Slider(value=0, minimum=0, maximum=1, step=0.1, label="Temperature")
                 with gr.Column(scale=5):
                     gr.ChatInterface(
                         fn=assistant.respond,
@@ -250,9 +123,10 @@ def main_loop():
                         retry_btn=None,
                         undo_btn=None,
                         submit_btn="Submit",
-                        # fill_height=True,
-                        # theme=gr.themes.Default(primary_hue="purple", secondary_hue="indigo"),
+                        fill_height=True,
+                        theme=gr.themes.Default(primary_hue="purple", secondary_hue="indigo"),
                     )
+            sl_temperature.change(assistant.change_temperature, sl_temperature)
             dd_model.change(assistant.change_model, dd_model)
             tb_file.upload(assistant.ingest_pdf, tb_file, show_progress='full')
             tb_file.clear(assistant.clear_pdf)
