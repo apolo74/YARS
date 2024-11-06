52a8c6204e654fb1733de79ad0824b11bc15b102
apolo74 <apolo74@gmail.com>
2024-11-06 15:51:05 +0100

    This is a final testing of the best performing version. It uses two chains for achieving the final goal. The first chain is in charge of generating an clean SQL query based on a list of semantic examples and the second chain executes the query and then translate the final result into natural language.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 ''' Chat with SQL database using Ollama 
-endpoints and LangChain/LangGraph libraries.
+endpoints and LangChain libraries.
 
 Notes:
     - Important -> Using Langchain V0.3.3!!!
-    - Important -> Works well with llama3.2:3b
+    - Important -> Works well with nemotron-mini
 
 Usage:    
     main.py [-h] [-s] examples
@@ -30,6 +30,7 @@ import argparse
 import langchain
 from langchain_ollama import ChatOllama, OllamaEmbeddings
 from langchain_community.utilities import SQLDatabase
+from langchain_community.tools.sql_database.tool import QuerySQLDataBaseTool
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_community.vectorstores import FAISS
@@ -37,10 +38,10 @@ from langchain_community.vectorstores import FAISS
 from langchain_community.agent_toolkits.sql.toolkit import SQLDatabaseToolkit
 from langchain_core.example_selectors import SemanticSimilarityExampleSelector
 from langchain_core.prompts import FewShotPromptTemplate, PromptTemplate
-from langchain_core.messages import HumanMessage
-from langgraph.prebuilt import create_react_agent
 
 from utils.load_config import LoadConfig
+
+from operator import itemgetter
 
 env = environ.Env()
 environ.Env.read_env('utils/.env')
@@ -136,31 +137,10 @@ def get_sql_query(llm, db, query_txt, examples_path):
     sql_query = extract_sql(raw_query)
 
     return sql_query
-    
-def get_agent_answer(llm, tools, sql_query):
-    # Create a react agent
-    system_prompt = """You are a helpful postgres agent named Burrito.
-        Your job is to respond in natural language using the results of executing the SQL query. 
-        Personal names should be enclosed between double quotes.
-    """
-    agent = create_react_agent(llm, tools, state_modifier=system_prompt)
-
-    events = agent.stream({"messages": [("user", sql_query)]}, stream_mode='messages')
-    for msg, metadata in events:
-        # print(f'[msg] {msg} \n[mData] {metadata}\n')
-        if (
-            msg.content
-            and not isinstance(msg, HumanMessage)
-            and metadata["langgraph_node"] == "agent"
-        ):
-            print(msg.content, end="", flush=True)
-    print()
-
-    return
 
 def main_loop( args ):
-    """Main loop: Using 'create_react_agent' and a basic prompt 
-                template plus a list of examples of pairs of questions 
+    """Main loop: Using two llm chains with guiding prompts
+                plus a list of examples of pairs of questions 
                 and their right sql queries.
     Args:
       ArgParse: a container for argument specifications
@@ -187,9 +167,10 @@ def main_loop( args ):
     # Setup database
     uri_conn = f"postgresql+psycopg2://{env('DB_USER')}:{env('DB_PASS')}@localhost:{env('DB_PORT')}/{env('DB_NAME')}"
     db = SQLDatabase.from_uri( uri_conn )
-    sql_toolkit = SQLDatabaseToolkit(db=db, llm=llm)
-    tools = sql_toolkit.get_tools()
-    
+
+    execute_query = QuerySQLDataBaseTool(db=db)
+    answer_prompt = PromptTemplate.from_template( APPCFG.template_answer )
+        
     try:
         while True:
             print(60 * '-', '\n')
@@ -198,7 +179,15 @@ def main_loop( args ):
             # Get the SQL query
             sql_query = get_sql_query(llm, db, query_txt, examples_path)
             if args.sql: print(f'[SQL] {sql_query}')
-            get_agent_answer(llm, tools, sql_query)
+            full_chain = (
+                RunnablePassthrough.assign( result=itemgetter("query") | execute_query )
+                | answer_prompt
+                | llm
+                | StrOutputParser()
+            )
+            for chunk in full_chain.stream( {"question": query_txt, "query": sql_query} ):
+                print(chunk, end="", flush=True)
+            print()
     except KeyboardInterrupt:
         print('Bye!')
     print()
@@ -222,3 +211,4 @@ if __name__ == '__main__':
     print(80 * '-')
     print("The end!".center(80))
     print(80 * '-')
+
